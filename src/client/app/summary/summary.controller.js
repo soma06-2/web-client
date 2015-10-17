0ab684bef3e5952c860763eb9661690d9c1c2079
beingbook@gmail.com <beingbook@gmail.com>
2015-10-18 00:22:56 +0900

    Update d3 graph initialized

--- src/client/app/summary/summary.controller.js
+++ src/client/app/summary/summary.controller.js
@@ -10,5 +10,23 @@
         var vm = this;
 
         vm.summary = summary;
+
+        console.log(summary);
+
+        var graph = d3
+            .select('.graph')
+            .append('svg:svg')
+            .attr('width', 900)
+            .attr('height', 450);
+
+        var yAxis = d3.scale.linear().domain([0, 1]).range([0, 400]);
+
+        graph
+            .append('svg:rect')
+            .attr('x', 100)
+            .attr('y', yAxis)
+            .attr('height', 100)
+            .attr('width', 20);
+
     }
 })();
